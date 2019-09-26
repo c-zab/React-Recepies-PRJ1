eee3ad3c3e869beeb1b5adf9a586e58a58e460e8
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-09-26 17:20:29 -0400

    Setting Up PostCSS and Basscss

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         loader: "babel-loader"
       },
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader'
