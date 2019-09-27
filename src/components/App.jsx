702293a3b9e41af6a77b4896ca1873892aa20df0
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-09-27 14:29:16 -0400

    Adding Navbar with NavLink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
 import Home from './Home.jsx';
 import Favorite from './Favorite.jsx';
 import Header from './Header.jsx';
@@ -10,6 +12,7 @@ const App = () => (
     <main>
       <Header />
       <Switch>
+        <Redirect from="/home" to="/" />
         <Route exact path="/" component={Home} />
         <Route path="/favorite" component={Favorite} />
         <Route component={NotFound} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,34 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    exact
+    className="p1 mx2 black rounded text-decoration-none"
+    activeClassName="bg-white"
+    {...props}
+  >
+    {children}
+  </NavLink>
+);
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex items-center justify-between px4">
     <h1 className="h1">
       <span role="img" aria-label="forks">🍽️</span>
       Header
     </h1>
+    <nav>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/favorite">Favorite</HeaderLink>
+    </nav>
   </header>
 );
+
+HeaderLink.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Header;
